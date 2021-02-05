371a83ed642643464f4ea3719596903b30bd17b2
Evan <evanfujita@gmail.com>
2021-02-04 22:19:23 -0600

    commit tree 2890f5e8531633d7282a77724f1c7899597f71ae

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your code here
+function submitData(name, email){
+
+    const info = {
+        name: name,
+        email: email
+    }
+    
+    const reqOb = {
+        'method': 'POST',
+        'headers': {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        'body': JSON.stringify(info)
+    }
+
+    fetch('http://localhost:3000/users', reqOb)
+        .then(resp => resp.json())
+        .then(data => {
+            console.log(data)
+        })
+        
+}
+
